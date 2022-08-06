aabf1915a9bba5f0fb46eadb47bc0ddae7c9647d
richardviana <richardpassostk@gmail.com>
2022-08-06 18:32:16 -0500

    criação do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/primeiraClasseJava.java
@@ -0,0 +1,9 @@
+package cursojava.executavel;
+
+public class primeiraClasseJava {
+	public static void main(String[] args) {
+		
+		/*Escreve texto no console*/
+		System.out.println("Meu primeiro programa em Java");
+	}
+}
