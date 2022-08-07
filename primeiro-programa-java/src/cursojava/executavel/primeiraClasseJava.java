ddcfd9e5d707a128eaf0cece1c6d7d40d074d30b
richardviana <richardpassostk@gmail.com>
2022-08-06 19:26:18 -0500

    teste

--- primeiro-programa-java/src/cursojava/executavel/primeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/primeiraClasseJava.java
@@ -4,6 +4,6 @@ public class primeiraClasseJava {
 	public static void main(String[] args) {
 		
 		/*Escreveee texto no console*/
-		System.out.println("Mudei Meu primeiro programa em Javaa");
+		System.out.println("Mudei Meu primeiro programa em Javaaa");
 	}
 }
