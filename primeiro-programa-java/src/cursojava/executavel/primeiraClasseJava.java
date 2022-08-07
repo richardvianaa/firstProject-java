38d9288c50ab9002e0248fd32ec3b2066ea6167b
richardviana <richardpassostk@gmail.com>
2022-08-06 19:02:38 -0500

    firts commit

--- primeiro-programa-java/src/cursojava/executavel/primeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/primeiraClasseJava.java
@@ -4,6 +4,6 @@ public class primeiraClasseJava {
 	public static void main(String[] args) {
 		
 		/*Escreve texto no console*/
-		System.out.println("Meu primeiro programa em Java");
+		System.out.println("Mudei Meu primeiro programa em Java");
 	}
 }
