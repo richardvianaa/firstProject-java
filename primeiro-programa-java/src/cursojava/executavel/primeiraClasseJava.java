d800196f2430b52bae02d9df3ada7d5d5405e648
richardviana <richardpassostk@gmail.com>
2022-08-06 19:12:27 -0500

    teste

--- primeiro-programa-java/src/cursojava/executavel/primeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/primeiraClasseJava.java
@@ -3,7 +3,7 @@ package cursojava.executavel;
 public class primeiraClasseJava {
 	public static void main(String[] args) {
 		
-		/*Escreve texto no console*/
-		System.out.println("Mudei Meu primeiro programa em Java");
+		/*Escreveee texto no console*/
+		System.out.println("Mudei Meu primeiro programa em Javaa");
 	}
 }
